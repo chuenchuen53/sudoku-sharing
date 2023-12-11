eae7b26e90c3825feea4c37f9ccf30c2cd49729f
chuenchuen53 <chuenchuen53@gmail.com>
2023-12-11 12:17:35 +0800

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
+    base: true,
+    styled: true,
+    utils: true,
   },
 };
